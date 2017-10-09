be196548153720c31706b01941fa41a63b6042cd
Carisa Tong <ctong1124@gmail.com>
2017-10-09 08:44:09 -0400

    end

--- src/board.js
+++ src/board.js
@@ -1,25 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex,columnIndex);
-    if(this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log("Game over!");
-      this._board.printBoard();
-    }
-    else if (!this._board.hasSafeTiles()){
-      console.log("You win!");
-    }
-    else {
-      console.log('Current board:');
-      this._board.printBoard();
-    }
-  }
-}
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = numberOfRows * numberOfColumns;
@@ -110,21 +89,3 @@ class Board {
   }
 
 }
-
-let g = new Game(3,3,3);
-g.playMove(0,0);
-
-
-
-
-// let playerBoard = generatePlayerBoard(3,4);
-// let bombBoard = generateBombBoard(3,4,5);
-//
-// console.log('Player board:')
-// printBoard(playerBoard);
-// console.log('Bomb board:')
-// printBoard(bombBoard);
-//
-// flipTile(playerBoard, bombBoard, 1, 0);
-// console.log('Updated Player Board:');
-// printBoard(playerBoard);
